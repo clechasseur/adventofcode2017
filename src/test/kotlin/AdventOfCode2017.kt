a28ed89f27b469fdd72ca7064098a030fefca7a8
Charles Lechasseur <charles.lechasseur@ingeno.ca>
2020-05-17 22:57:40 -0400

    Day 8

--- src/test/kotlin/AdventOfCode2017.kt
+++ src/test/kotlin/AdventOfCode2017.kt
@@ -85,4 +85,16 @@ class AdventOfCode2017 {
             assertEquals(362, Day7.part2())
         }
     }
+
+    class Day8Puzzles {
+        @Test
+        fun `day 8, part 1`() {
+            assertEquals(4832, Day8.part1())
+        }
+
+        @Test
+        fun `day 8, part 2`() {
+            assertEquals(5443, Day8.part2())
+        }
+    }
 }
